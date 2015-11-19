d331b3f4ce60d77db95871942b84448d3ba48118
8108_web (Student) <s_8108_web@PEDAGOGIQUE.LAN>
2015-11-19 16:02:29 +0100

    module

--- ex4/index.js
+++ ex4/index.js
@@ -3,8 +3,8 @@ var utils = require('../modules/utils');
 fs.readdir('../', function(err, files) {
   if (err) throw err;
   fs.writeFile('result.txt',
-  createNiceListofFiles(files), function(err, data)
-  {
+  utils.createNiceListofFiles(files),
+  function(err, data) {
       if (err) throw err;
       console.log("great");
       });
